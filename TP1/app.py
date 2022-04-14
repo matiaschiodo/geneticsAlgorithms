00a635c0eb2620907e452345e12f954ae7941b35
exe72418 <72991301+exe72418@users.noreply.github.com>
2022-04-14 19:15:04 -0300

    Add files via upload

--- TP1/app.py
+++ TP1/app.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 def inicializarPoblacion(poblacion, cromosomas):
   return [[random.randint(0, 1) for i in range(cromosomas)] for i in range(poblacion)]
@@ -10,11 +11,34 @@ def objective(cromosoma):
 def fitness(poblacion):
   obj = []
   fit = []
+  suma = 0
   for i in range(len(poblacion)):
     obj.append(objective(poblacion[i]))
   for i in range(len(poblacion)):
-    fit.append(round(obj[i] / sum(obj), 4))
+    fit.append(obj[i] / sum(obj))
+  grafica(fit)
   return fit
+
+def grafica(porcentas):
+#DEFINIMOS ETIQUETAS  
+  etiquetas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'] #labels
+
+  #PORCENTAJE DE CADA PORCIÓN. (parametro=porcentas)
+
+  #DEFIMIMOS COLORES
+  colores = ['#1abc9c', '#f1c40f', '#8e44ad', '#e74c3c', '#34495e', '#3498db', '#FFFF00', '#808000', '#32a852', '#008000'] #LabelColor
+
+  #DIBUJAMOS GRÁFICA.  
+  plt.pie(porcentas, labels = etiquetas, colors=colores,
+          startangle=90, explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1,0.1, 0.1, 0.1,0.1),
+          radius = 1.2, autopct = '%1.2f%%')
+
+  #TITULO
+  plt.title('Gráfica Circular')
+
+  #MOSTRAMOS GRÁFICA.
+  plt.show()
+  
 
 def arrayToInt(array):
   string = str(array[0])
